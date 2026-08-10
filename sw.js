440b8bde918cd4c11f7f28fced485760141a85bc
mas-gondrong <ummayah181@gmail.com>
2026-08-10 23:29:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mas-gondrong-v7';
+const CACHE_NAME = 'mas-gondrong-v8';
 const urlsToCache = [
   'index.html',
   'admin.html',
@@ -20,4 +20,32 @@ self.addEventListener('fetch', event => {
         return response || fetch(event.request);
       })
   );
+});
+
+// Import script Firebase Messaging untuk Service Worker
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js");
+
+// Initialize Firebase di SW
+const firebaseConfig = {
+  apiKey: "AIzaSyCtzYShY70eQGLNUnoQCi91uy7aMuFsYsc",
+  authDomain: "mas-gondrong.firebaseapp.com",
+  projectId: "mas-gondrong",
+  storageBucket: "mas-gondrong.firebasestorage.app",
+  messagingSenderId: "191854522046",
+  appId: "1:191854522046:web:61bed6d89182392044b8ef"
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Background handler untuk notifikasi
+messaging.onBackgroundMessage((payload) => {
+  console.log('[sw.js] Notifikasi diterima di background', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'logo-192.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
